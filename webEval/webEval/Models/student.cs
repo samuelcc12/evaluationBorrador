820355812f3f137bf198846b762267f4966116a1
samuel.cabrera.1999@gmail.com <samuel.cabrera.1999@gmail.com>
2019-09-26 20:06:35 -0400

    creacion del modelo

---
+++ webEval/webEval/Models/student.cs
@@ -0,0 +1,28 @@
+﻿namespace webEval.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public enum Gender
+    {
+        male,
+        female
+    }
+
+    public class student
+    {
+        public int scoreID { get; set; }
+
+        public string name { get; set; }
+
+        public Gender Sex { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Birthday { get; set; }
+
+
+
+
+    }
+}
